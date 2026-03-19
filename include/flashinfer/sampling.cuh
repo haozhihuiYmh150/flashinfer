bc8571ec3dc1c0b7479263601385aebf88c7018c
goodluck <8744786+goodluck-lichongguang@user.noreply.gitee.com>
2026-03-19 11:01:02 +0800

    fix: fix compile error

--- include/flashinfer/sampling.cuh
+++ include/flashinfer/sampling.cuh
@@ -1297,7 +1297,7 @@ __global__ void GetTopKTopPFilteredProbKernel(DType* probs, DType* filtered_prob
                 indices, top_k_val,
                 top_p_val, d, philox_seed,
                 philox_offset,
-                d);
+                float(d));
 }
 
 template <uint32_t BLOCK_THREADS, BlockScanAlgorithm SCAN_ALGORITHM,
