80de6885b15206c2d8d0b41e79a426e803fb3070
goodluck <8744786+goodluck-lichongguang@user.noreply.gitee.com>
2026-03-24 19:37:52 +0800

    fix: update test

--- tests/test_return_probs.py
+++ tests/test_return_probs.py
@@ -230,7 +230,7 @@ def test_top_k_top_p_sampling_acc(batch_size=1):
     print(f'{diff_golden=}')
 
 if __name__ == "__main__":
-    torch.set_printoptions(threshold=float('inf'))
+    torch.set_printoptions(threshold=float('inf'), precision=6)
     # test_top_k_top_p_sampling_performance(1)
     # test_top_k_top_p_sampling_performance(16)
     # test_top_k_top_p_sampling_performance(32)
