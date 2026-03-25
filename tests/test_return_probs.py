529d7d6538f24e977e6e42fdcccdb7f43ed187f3
goodluck <8744786+goodluck-lichongguang@user.noreply.gitee.com>
2026-03-25 12:25:23 +0800

    fix: cluster bug

--- tests/test_return_probs.py
+++ tests/test_return_probs.py
@@ -229,10 +229,10 @@ def test_top_k_top_p_sampling_acc(batch_size=1):
     print(f'{diff_golden=}')
 
 if __name__ == "__main__":
-    torch.set_printoptions(threshold=float('inf'), precision=6)
-    test_top_k_top_p_sampling_acc(1)
+    # torch.set_printoptions(threshold=float('inf'), precision=6)
+    # test_top_k_top_p_sampling_acc(1)
 
-    # test_top_k_top_p_sampling_performance(1)
+    test_top_k_top_p_sampling_performance(1)
     # test_top_k_top_p_sampling_performance(16)
     # test_top_k_top_p_sampling_performance(32)
     # test_top_k_top_p_sampling_performance(64)
