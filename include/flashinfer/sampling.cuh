7cf245dc856ac22b76c0daebd7af520711fe29ef
goodluck <8744786+goodluck-lichongguang@user.noreply.gitee.com>
2026-04-11 21:37:51 +0800

    fix: tuned param

--- include/flashinfer/sampling.cuh
+++ include/flashinfer/sampling.cuh
@@ -2022,7 +2022,7 @@ cudaError_t GetTopKTopPFilteredProb(T* probs, IdType* top_k_arr, T* top_p_arr, T
                 probs, filtered_probs, top_k_arr, top_p_arr,
                 indices, top_k_val, top_p_val, d, philox_seed, philox_offset));
         } else {
-          constexpr uint32_t BLOCK_THREADS = 1024;
+          constexpr uint32_t BLOCK_THREADS = 512;
           constexpr int cluster_size = 4;
           const uint32_t smem_size = sizeof(GetTopKTopPFilteredProbSmemLayout<BLOCK_THREADS, SCAN_ALGO, REDUCE_ALGO, PIVOTS_PER_BLOCK>);
           auto kernel = GetTopKTopPFilteredProbKernel<BLOCK_THREADS, SCAN_ALGO, REDUCE_ALGO,
